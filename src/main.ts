a303a96dc3413d987fab589e113888f886055810
C1325300 Gabriel Dos Santos Motroni <gabrielmotroni@ibm.com>
2024-01-19 09:31:19 -0300

    add swager

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API Nestjs - Gestão de Usuários e Produtos')
+    .setDescription('Api para gerir e cadastrar usuário e produtos')
+    .setVersion('1.0')
+    .addTag('gestao')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       //permite transformar os dados json para a nossa classe DTO
